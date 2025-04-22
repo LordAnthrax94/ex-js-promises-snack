ab900190cfc8efc3eb3855ac6f710018168a2f20
LordAnthrax94 <andreagi1994@gmail.com>
2025-04-22 16:05:16 +0200

    feat: inserito snack 2 e completato snack 1

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// SNACK 1
 // Ottieni il titolo di un post con una Promise.
 
 // Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
@@ -19,3 +20,9 @@ function getTitle(id){
 getTitle(1)
 .then(title => console.log('Il titolo del post é:', title))
 .catch(error => console.log(error))
+
+
+// SNACK 2
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
